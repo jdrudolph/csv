ac1a2973382ffe8d9d3afe238c5a99420f9d6c97
jandanielr <jan.daniel.rudolph@gmail.com>
2014-02-26 18:09:51 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='csv',
+		version='0.2',
+		scripts=['csv'],
+
+		author='Jan Rudolph',
+		license='MIT license',
+		author_email='jan.daniel.rudolph@gmail.com',
+		description='extract a column from a csv file',
+		long_description=open('README').read(),
+		)
